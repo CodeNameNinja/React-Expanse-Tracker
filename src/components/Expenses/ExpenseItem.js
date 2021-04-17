5e7663e910e63fbea9150b2fd225bfdefd5b6703
Mitchell Yuen <mitchell.yuen@lavalamp.biz>
2021-04-17 17:24:17 +0200

    added NewExpense and ExpenseForm components
    These component render the feature to add a new expense

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,22 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 const ExpenseItem = (props) => {
-  const expenseTitle = props.title;
-  const expenseAmount = props.amount;
+  // const [title] = useState(props.title);
+
+  const amount = props.amount;
 
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
+
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
