4a6c7fc86c2388f3e9da8431068b6166d96babc6
Mitchell Yuen <mitchell.yuen@lavalamp.biz>
2021-04-17 17:38:22 +0200

    using useState in ExpenseForm

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -14,7 +14,6 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-
     </Card>
   );
 };
